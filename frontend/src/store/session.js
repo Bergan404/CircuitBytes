db76116d375dbc5ab6d1327305f08646ab52b4ea
Bergan Oudshoorn <Bergan00@gmail.com>
2021-03-27 11:15:52 -0400

    frontend signup

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -16,6 +16,13 @@ const removeUser = () => {
   };
 };
 
+export const restoreUser = () => async dispatch => {
+    const response = await csrfFetch('/api/session');
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+
 export const login = (user) => async (dispatch) => {
   const { credential, password } = user;
   const response = await csrfFetch('/api/session', {
@@ -29,6 +36,21 @@ export const login = (user) => async (dispatch) => {
   dispatch(setUser(data.user));
   return response;
 };
+
+export const signup = (user) => async (dispatch) => {
+    const { username, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
 
 const initialState = { user: null };
 
