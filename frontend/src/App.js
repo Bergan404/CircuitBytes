f1f706e7c7d00ce433a6700a3619f4c9e586a0aa
Bergan Oudshoorn <Bergan00@gmail.com>
2021-03-31 08:56:32 -0400

    getting up and running

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
+import LoginFormPage from './components/LoginFromPage'
+import SignupFormPage from './components/SignupFromPage'
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
 import HomePage from './components/HomePage/HomePage'
@@ -13,27 +15,48 @@ import PostProperties from './components/PostProperties/index'
 function App() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
-
   useEffect(() => {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
   }, [dispatch]);
+
   return (
     <>
       <Navigation isLoaded={isLoaded} />
+      {isLoaded && (
         <Switch>
-          <Route path="/" exact component={HomePage} />
+          <Route path="/login">
+            <LoginFormPage />
+          </Route>
 
-          <Route path="/profile/:username" component={ProfilePage} />
+          <Route path="/signup">
+            <SignupFormPage />
+          </Route>
 
-          <Route path="/create-post" component={Posts}/>
+          <Route path="/" exact component={HomePage}>
+            <HomePage />
+          </Route>
 
-          <Route path="/post" component={PostOpened}/>
+          <Route path="/profile/:username" component={ProfilePage}>
+            <ProfilePage />
+          </Route>
 
-          <Route path="/shopping-cart/:username" component={ShoppingCart}/>
+          <Route path="/create-post" component={Posts}>
+            <Posts />
+          </Route>
 
-          <Route path="/post/:id" component={PostProperties}/>
+          <Route path="/post" component={PostOpened}>
+            <PostOpened />
+          </Route>
 
+          <Route path="/shopping-cart/:username" component={ShoppingCart}>
+            <ShoppingCart />
+          </Route>
+
+          <Route path="/post/:id" component={PostProperties}>
+            <PostProperties />
+          </Route>
         </Switch>
+      )}
     </>
   );
 }
