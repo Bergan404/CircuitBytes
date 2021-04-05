588a05be8b094315b9f17efa64c342f3bc8b11e5
Bergan Oudshoorn <Bergan00@gmail.com>
2021-04-05 11:33:07 -0400

    removed redirect

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
-import {NavLink, Redirect, useHistory} from 'react-router-dom'
+import {NavLink, useHistory} from 'react-router-dom'
 
 import * as sessionActions from '../../store/session';
 
